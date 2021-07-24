8091ec5d1df6963f75bbe04d310758d28317b02c
kopianan <ananalfredcarlos@gmail.com>
2021-07-24 10:45:39 +0700

    test

---
+++ lib/row/row_2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Row2 extends StatefulWidget {
+  const Row2({ Key? key }) : super(key: key);
+
+  @override
+  _Row2State createState() => _Row2State();
+}
+
+class _Row2State extends State<Row2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
