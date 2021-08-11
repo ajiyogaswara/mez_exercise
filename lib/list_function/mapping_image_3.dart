e963f57fb8064dc0ba9e5198599df5acb9459d62
Aji Dwi Yogaswara <ajiyogaswara31@gmail.com>
2021-08-11 22:10:07 +0700

    lib/list_function/mapping_image_3.dart: unfinished work.

--- lib/list_function/mapping_image_3.dart
+++ lib/list_function/mapping_image_3.dart
@@ -10,6 +10,8 @@ class MappingImage3 extends StatefulWidget {
 class _MappingImage3State extends State<MappingImage3> {
   List<DataClass> dataClass = <DataClass>[];
 
+  var children;
+
   @override
   void initState() {
     initDataClass();
@@ -17,11 +19,14 @@ class _MappingImage3State extends State<MappingImage3> {
   }
 
   void initDataClass() {
-    dataClass.add(DataClass("https://unsplash.com/photos/IhbQOvsbgMk",
+    dataClass.add(DataClass(
+        "https://images.unsplash.com/photo-1628285477029-e98c852cfb63?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         "Ini Adalah Kalimat Pertama"));
     dataClass.add(DataClass(
-        "https://unsplash.com/photos/ZKchcS43QJU", "Ini Adalah Kalimat Kedua"));
-    dataClass.add(DataClass("https://unsplash.com/photos/3eACKATUEm0",
+        "https://images.unsplash.com/photo-1593642634315-48f5414c3ad9?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "Ini Adalah Kalimat Kedua"));
+    dataClass.add(DataClass(
+        "https://images.unsplash.com/photo-1628154439767-844031fb03f8?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         "Ini Adalah Kalimat Ketiga"));
   }
 
@@ -29,16 +34,21 @@ class _MappingImage3State extends State<MappingImage3> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            dataClass.map((e) {
-              return e.forEach((element) {
-                Image.network(element.image);
-                element.text;
-              });
-            }).toList();
-          ],
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              Column(
+                  children: dataClass.map((e) {
+                return Container(
+                    height: 100, width: 100, child: Image.network(e.image));
+              }).toList()),
+              Column(
+                  children: dataClass.map((e) {
+                return Container(height: 100, width: 100, child: Text(e.text));
+              }).toList()),
+            ],
+          ),
         ),
       ),
     );
